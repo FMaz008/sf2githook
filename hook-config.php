7de35b3876db56a0a3f302b543d1584826ca7ed2
fmazerolle <fmazerolle@idealtechnology.net>
2011-11-07 16:06:48 -0500

    First draft of the pre-commit hook.

---
+++ hook-config.php
@@ -0,0 +1,11 @@
+<?php
+
+//Path to PHPUnit
+define('PHPUNIT_PATH', '/usr/local/Cellar/php/5.3.8/bin/phpunit');
+
+//Path to PHP Code Sniffer
+define('PHPCS_PATH', '/usr/local/Cellar/php/5.3.8/bin/phpcs');
+
+//Path to PHP
+define('PHP_PATH', '/usr/local/Cellar/php/5.3.8/bin/php');
+
